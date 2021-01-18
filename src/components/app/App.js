1f4488828c1d85add6cb15c4854f915123c9271e
coffeemaniak <derrykain@mail.ru>
2021-01-18 22:21:06 +0300

    join window

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import {Route, Switch} from "react-router-dom";
+import {Route, Router, Switch} from "react-router-dom";
 import Carousel from "../slider";
 import Header from "../header";
 import MainSection from "../main-section";
-import {Catalog, About, Login} from "../pages";
+import {Catalog, About, Login,  Join} from "../pages";
 import BookList from "../book-list";
 
 
@@ -21,6 +21,7 @@ function App  () {
       </Switch>
       <Switch>
         <Route path="/login" component={Login}/>
+        <Route path="/join" component={Join}/>
       </Switch>
     </div>
     
--- src/components/pages/join.js
+++ src/components/pages/join.js
@@ -1,8 +1,22 @@
 import React from "react";
+import "./authorization.css"
 
 const Join = () => {
     return (
-        <h1>Join</h1>
+        <div className="join_block">
+            <form>
+            <label className="join_email">
+                <input type="email" className="join_input" placeholder="E-mail"/>
+            </label>
+            <label className="join_password">
+                <input type="password" className="join_input" placeholder="Password"/>
+            </label>
+            <label className="join_password">
+                <input type="password" className="join_input" placeholder="Confirm password"/>
+            </label>
+            <button type="submit" className="join_enter">Зарегистрироваться</button>
+            </form>
+        </div>
     )
 }
 
